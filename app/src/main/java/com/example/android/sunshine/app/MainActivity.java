1c087fae2a30e0569e7e158478cfdf6ca27687a8
sandesh.achari <sandesh.achari@btech2010.iitgn.ac.in>
2015-12-06 23:25:52 +0530

    Json parsing not successful

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.text.FieldPosition;
-import java.util.jar.Manifest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
         //int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.perrmission.ACCESS_WIFI_STATE);
@@ -41,6 +44,8 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -57,42 +62,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            final ListView listView;
-            listView = (ListView)rootView.findViewById(R.id.list_item);
-            String[] values = new String[]{"Name1",
-                    "Name2",
-                    "Name3",
-                    "Name4",
-                    "Name5",
-                    "Name6",
-                    "Name7",
-                    "Name8",
-
-            };
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,values);
-            listView.setAdapter(adapter);
-
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-                public void onItemClick(AdapterView<?>parent,View view,int position, long id){
-                    int itemPosition = position;
-                    String itemValue = (String)listView.getItemAtPosition(position);
-                    Toast.makeText(getActivity(), "Position: "+itemPosition+"ListItem: "+itemValue, Toast.LENGTH_SHORT).show();
-                }
-            });
-            return rootView;
-        }
-    }
 }
