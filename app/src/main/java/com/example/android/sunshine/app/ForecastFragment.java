51c8aa9aa68435a82d0841030d03e46d573a535e
sandesh.achari <sandesh.achari@btech2010.iitgn.ac.in>
2015-12-07 00:26:35 +0530

    working code for json parsing, adapter can update. Yesss!

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -16,10 +16,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,25 +30,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
+    private ArrayAdapter<String> mForecasrAdapter;
     private final String LOG_TAG =  FetchWeatherTask.class.getSimpleName();
     int numDays = 7;
-    String [] values;
-    /*
-    String[] values = new String[]{"Name1",
-            "Name2",
-            "Name3",
-            "Name4",
-            "Name5",
-            "Name6",
-            "Name7"
-    };
-    */
+
 
     public ForecastFragment() {
     }
@@ -71,19 +62,8 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
         if(id == R.id.action_refresh){
-            //Toast.makeText(getActivity(), "Refresh Selected", Toast.LENGTH_SHORT).show();
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            //String result = null;
-            try {
-                values = weatherTask.execute("94043").get();
-            }catch (InterruptedException e){
-                Log.e(LOG_TAG,"InterruptedException");
-            }catch (ExecutionException e){
-                Log.e(LOG_TAG, "ExecutionException");
-            }
-
-           // values = this.getWeatherDataFromJson(result,numDays);
-
+             weatherTask.execute("Mumbai,IN");  //94043
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -93,31 +73,23 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        final ListView listView;
-        listView = (ListView)rootView.findViewById(R.id.list_item_forecast);
-        /*
-        String[] values = new String[]{"Name1",
-                "Name2",
-                "Name3",
-                "Name4",
-                "Name5",
-                "Name6",
-                "Name7",
-                "Name8",
 
+
+        String[] values = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
         };
-        */
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,values);
-        listView.setAdapter(adapter);
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(values));
+        mForecasrAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,weekForecast);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.list_item_forecast);
+        listView.setAdapter(mForecasrAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition = position;
-                String itemValue = (String) listView.getItemAtPosition(position);
-                Toast.makeText(getActivity(), "Position: " + itemPosition + "ListItem: " + itemValue, Toast.LENGTH_SHORT).show();
-            }
-        });
         return rootView;
     }
 
@@ -138,7 +110,6 @@ public class ForecastFragment extends Fragment {
         // For presentation, assume the user doesn't care about tenths of a degree.
         long roundedHigh = Math.round(high);
         long roundedLow = Math.round(low);
-
         String highLowStr = roundedHigh + "/" + roundedLow;
         return highLowStr;
     }
@@ -317,6 +288,13 @@ public class ForecastFragment extends Fragment {
                 e.printStackTrace();
                 return null;
             }
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+            mForecasrAdapter.clear();
+            mForecasrAdapter.addAll(strings);
         }
     }
 }
